2b12ddda87996262f9791915d2609b904faf068b
ymewangyc <wangyongchao_go@163.com>
2019-04-12 16:34:03 +0800

    frist commit

---
+++ assets/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from assets import views
+
+app_name = 'assets'
+
+urlpatterns = [
+    url(r'^report/', views.report, name='report'),
+]
---
+++ assets/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+@csrf_exempt
+def report(request):
+    if request.method == "POST":
+        asset_data = request.POST.get('asset_data')
+        print(asset_data)
+        return HttpResponse("成功收到数据！")
